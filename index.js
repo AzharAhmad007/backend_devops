62c05cba0836419ce552883a49eaa89d1d264c06
I529022 <azhar.ahmad02@sap.com>
2025-03-04 16:49:35 +0530

    cors configure

--- index.js
+++ index.js
@@ -10,7 +10,12 @@ const app = express();
 
 app.use(bodyParser.json({ limit: '30mb', extended: true }))
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
-app.use(cors());
+app.use(cors({
+  origin: '*', // Allow all origins (not recommended for production)
+  methods: ['GET', 'POST', 'PATCH', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 
 app.use('/posts', postRoutes);
 
